1e63fc8ba1c1cef07fe5a7572b3405a4c9dc888c
Yoan Hillion <yoan.hillion@hotmail.fr>
2020-04-11 11:43:40 +0200

    add analytics and change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,9 +50,22 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `src/images/icon.svg` // This path is relative to the root of the site.
+        icon: `src/images/favicon2.svg` // This path is relative to the root of the site.
       }
     },
-    `gatsby-plugin-offline`
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-163369745-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true
+      }
+    }
   ]
 }
